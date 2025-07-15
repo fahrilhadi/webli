eea86518e8b5d6ed2473678341d81ee36df09e73
Fahril Hadi <fahrilhadi77@gmail.com>
2025-07-15 17:09:59 +0700

    laravel multi auth with breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +19,12 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+    // group route with middleware "auth,role:admin"
+    Route::middleware(['auth','role:admin'])->group(function () {
+        // dashboard
+        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
+    });
+});
